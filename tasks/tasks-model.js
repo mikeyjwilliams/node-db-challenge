57893ce0f0ee9dbd09148b604974bda868d2aabf
Michael Williams <manicdesigner@gmail.com>
2020-03-07 14:28:11 -0500

    built getTasks method

--- tasks/tasks-model.js
+++ tasks/tasks-model.js
@@ -3,6 +3,7 @@ const db = require('../data/config');
 module.exports = {
   getById,
   addTask,
+  getTasks,
 };
 
 function getById(id) {
@@ -16,4 +17,17 @@ async function addTask(task) {
   const [id] = await db('tasks').insert(task);
 
   return getById(id);
+}
+
+function getTasks(project_id) {
+  return db('tasks as t')
+    .select(
+      'p.name as project_name',
+      'p.description as project_description',
+      't.description as task_description',
+      't.notes as task_notes',
+      't.completed as task_completed'
+    )
+    .join('projects as p', 't.project_id', 'p.id')
+    .where('t.project_id', project_id);
 }
