fa1b697adb73334b89e3dd1db65bfbd953224e87
Michael Williams <manicdesigner@gmail.com>
2020-03-07 13:36:25 -0500

    complete the post  in project-router for posting a project

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const projectModel = require('./projects-model');
 
 router.post('/', async (req, res, next) => {
   const { name } = req.body;
   if (!name) {
     return res.status(400).json({ message: 'name of project is required' });
+  }
+  const projectInput = {
+    name: name,
+    description: req.body.description || null,
+    completed: req.body.completed || 0,
+  };
+  try {
+    const project = await projectModel.addProject(projectInput);
+    res.status(201).json(project);
+  } catch (err) {
+    next(err);
   }
 });
 
